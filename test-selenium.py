83df7f9fa08d6d5a3f688d85e81de9abe97f616a
Gusti Agung Ngurah <71577368+cap10jack@users.noreply.github.com>
2023-02-14 12:07:47 +0700

    Update test-selenium.py

--- test-selenium.py
+++ test-selenium.py
@@ -12,7 +12,7 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("http://localhost:80/index.php")
+driver.get("http://pluto18.epizy.com/index.php")
 time.sleep(10)
 # delay = 10 # seconds
 # try:
@@ -20,103 +20,97 @@ time.sleep(10)
 #     print("Page is ready!")
 # except Exception as e:
 #     print(driver.page_source)
-print("Test")
-input_elements = driver.find_elements("xpath",'//input')   
-for input_element in input_elements:
-    input_id = input_element.get_attribute("id")
-    if input_id:
-        print(input_id)
-        
-# Scenario 1: Test successful login
-# username = driver.find_element("xpath","//input[@name='username']")
-# password = driver.find_element("id","password")
 
-# username.send_keys("admin")
-# password.send_keys("admin123")
+Scenario 1: Test successful login
+username = driver.find_element("xpath","//input[@name='username']")
+password = driver.find_element("id","password")
 
-# driver.find_element("xpath","//input[@type='submit']").click()
-# time.sleep(2)
+username.send_keys("admin")
+password.send_keys("admin123")
 
-# if driver.current_url == "http://localhost/home.php":
-#     print("Scenario 1: Testing Successful")
-# else:
-#     print("Scenario 1: Testing Failed")
+driver.find_element("xpath","//input[@type='submit']").click()
+time.sleep(2)
 
-# # Scenario 2: Test wrong username
-# driver.back()
+if driver.current_url == "http://pluto18.epizy.com/home.php:
+    print("Scenario 1: Testing Successful")
+else:
+    print("Scenario 1: Testing Failed")
 
-# username = driver.find_element("id","username")
-# password = driver.find_element("id","password")
+# Scenario 2: Test wrong username
+driver.back()
 
-# username.clear()
-# password.clear()
+username = driver.find_element("id","username")
+password = driver.find_element("id","password")
 
-# username.send_keys("notadmin")
-# password.send_keys("admin123")
+username.clear()
+password.clear()
 
-# driver.find_element("xpath","//input[@type='submit']").click()
-# time.sleep(2)
+username.send_keys("notadmin")
+password.send_keys("admin123")
 
-# notif = driver.find_element("xpath","//div[@class='notif']").text
+driver.find_element("xpath","//input[@type='submit']").click()
+time.sleep(2)
 
-# if notif == "Wrong username or password!":
-#     print("Scenario 2: Testing Successful")
-# else:
-#     print("Scenario 2: Testing Failed")
+notif = driver.find_element("xpath","//div[@class='notif']").text
 
-# # Scenario 3: Test wrong password
-# driver.back()
+if notif == "Wrong username or password!":
+    print("Scenario 2: Testing Successful")
+else:
+    print("Scenario 2: Testing Failed")
 
-# username = driver.find_element("id","username")
-# password = driver.find_element("id","password")
+# Scenario 3: Test wrong password
+driver.back()
 
-# username.clear()
-# password.clear()
+username = driver.find_element("id","username")
+password = driver.find_element("id","password")
 
-# username.send_keys("admin")
-# password.send_keys("notadmin123")
+username.clear()
+password.clear()
 
-# driver.find_element("xpath","//input[@type='submit']").click()
-# time.sleep(2)
+username.send_keys("admin")
+password.send_keys("notadmin123")
 
-# notif = driver.find_element("xpath","//div[@class='notif']").text
+driver.find_element("xpath","//input[@type='submit']").click()
+time.sleep(2)
 
-# if notif == "Wrong username or password!":
-#     print("Scenario 3: Testing Successful")
-# else:
-#     print("Scenario 3: Testing Failed")
+notif = driver.find_element("xpath","//div[@class='notif']").text
 
-# # Scenario 4: Test empty input
-# driver.back()
+if notif == "Wrong username or password!":
+    print("Scenario 3: Testing Successful")
+else:
+    print("Scenario 3: Testing Failed")
 
-# username = driver.find_element("id","username")
-# password = driver.find_element("id","password")
+# Scenario 4: Test empty input
+driver.back()
 
-# username.clear()
-# password.clear()
+username = driver.find_element("id","username")
+password = driver.find_element("id","password")
 
-# driver.find_element("xpath","//input[@type='submit']").click()
-# time.sleep(2)
-# if driver.current_url == "http://localhost/index.php":
-#     print("Scenario 4: Testing Successful")
-# else:
-#     print("Scenario 4: Testing Failed")
+username.clear()
+password.clear()
 
-# # Scenario 5 : Test Karakter tidak valid
+driver.find_element("xpath","//input[@type='submit']").click()
+time.sleep(2)
+if driver.current_url == "http://localhost/index.php":
+    print("Scenario 4: Testing Successful")
+else:
+    print("Scenario 4: Testing Failed")
 
-# time.sleep(10)
-# username = driver.find_element("xpath","//input[@id='username']")
-# password = driver.find_element("xpath","//input[@id='password']")
-# username.clear()
-# password.clear()
+# Scenario 5 : Test Karakter tidak valid
 
-# username.send_keys("\x01\x02")
-# password.send_keys("\x01")
+time.sleep(10)
+username = driver.find_element("xpath","//input[@id='username']")
+password = driver.find_element("xpath","//input[@id='password']")
+username.clear()
+password.clear()
 
-# driver.find_element("xpath","//input[@type='submit']").click()
+username.send_keys("\x01\x02")
+password.send_keys("\x01")
 
-# time.sleep(2)
-# if driver.current_url == "http://localhost/index.php":
-#     print("Scenario 5: Testing Successful")
-# else:
-#     print("Scenario 5: Testing Failed")
+driver.find_element("xpath","//input[@type='submit']").click()
+
+time.sleep(2)
+if driver.current_url == "http://localhost/index.php":
+    print("Scenario 5: Testing Successful")
+else:
+    print("Scenario 5: Testing Failed")
